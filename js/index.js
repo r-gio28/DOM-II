950b9236c316a744e562861a3b9027f2ffc5f08f
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-09-28 19:55:03 -0500

    still working almost finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
+
 // Your code goes here
+
+//mouseover
+
+const navBar = document.querySelector('nav');
+
+navBar.addEventListener('mouseover', function(event){
+    event.target.style.color = 'red';
+    setTimeout (function(){
+        event.target.style.color = '';
+    }, 500);
+}, false);
+
+//keydown
+
+window.addEventListener('keydown', function(){
+    alert('PUSH DAT BUTTON');
+})
+
+
+//mouseover
+
+const footer = document.querySelector('footer');
+footer.addEventListener('mouseover', function(e){
+    console.log(`howdy partner`);
+    e.stopPropagation();
+});
+
+//3wheel
+
+const foot = document.querySelector('footer');
+    foot.addEventListener('wheel', function(e){
+    console.log(`did ya scroll past me ,bud? ${e}`);
+    e.stopPropagation();
+
+});
+
+//aux click
+
+const anchors = document.querySelector('.nav a');
+anchors.forEach(function(e){
+    e.addEventListener('auxclick', function(){
+        e.style.color = 'blue';
+        console.log('turning the links blue')
+    });
+})
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
